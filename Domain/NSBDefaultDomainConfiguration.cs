fd5ace1b688899c5f79af2e6de97417e9b591aba
pablocastilla <pablo.castilla@gmail.com>
2015-01-22 08:49:11 +0100

    SessionFactoryCreator abstracted

--- Domain/NSBDefaultDomainConfiguration.cs
+++ Domain/NSBDefaultDomainConfiguration.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Entities;
 using Domain.Repositories;
 using NServiceBus;
 using NServiceBus.UnitOfWork;
@@ -24,7 +25,7 @@ namespace Domain
                     cfg.For<IDomainUnitOfWork>().Use<DomainUnitOfWork>();
                     cfg.Forward<IDomainUnitOfWork,IUnitOfWork>();
                     cfg.Policies.FillAllPropertiesOfType<IUnitOfWork>();
-                    cfg.For<ISessionFactoryCreator>().Use<DomainSessionFactoryCreator>().Singleton(); 
+                    cfg.For<ISessionFactoryCreator>().Use<AbstractSessionFactoryCreator<A>>().Ctor<string>("connectionString").Is("Data Source=localhost:1521/xe;Persist Security Info=True;User ID=CONCEPT;Password=CONCEPT").Singleton(); 
 
                                              
                 }
---
+++ Shared/NHibernate/AbstractSessionFactoryCreator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using Shared.NHibernate;
+
+namespace Shared.NHibernate
+{
+    /// <summary>
+    /// Creates a session factory for a model.
+    ///  cfg.For<ISessionFactoryCreator>().Use<AbstractSessionFactoryCreator<A>>().Ctor<string>("connectionString").Is("Data Source=localhost:1521/xe;Persist Security Info=True;User ID=CONCEPT;Password=CONCEPT").Singleton(); 
+    /// </summary>
+    /// <typeparam name="T">An entity of the domain for this session</typeparam>
+    public class AbstractSessionFactoryCreator<T> : ISessionFactoryCreator
+    {
+        private global::NHibernate.Cfg.Configuration configuration;
+        private static Object lockO = new Object();
+        private string connectionString;
+
+        private ISessionFactory sessionFactory;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connectionStringName">Name of the connection string in the config.</param>
+        public AbstractSessionFactoryCreator(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public ISessionFactory GetSessionFactory()
+        {
+            lock (lockO)
+            {
+                if (sessionFactory == null)
+                {
+                    configuration = Fluently.Configure()
+                       .Database(OracleDataClientConfiguration.Oracle10
+                       .ConnectionString(connectionString)
+                       .Dialect<Oracle10gDialect>()
+                       .Driver<OracleManagedDataClientDriver>()
+                       )
+                       .Mappings(m => m.FluentMappings.AddFromAssemblyOf<T>()).BuildConfiguration();
+
+                    new global::NHibernate.Tool.hbm2ddl.SchemaExport(configuration).Execute(false, true, false);
+
+                    sessionFactory = configuration.BuildSessionFactory();
+                }
+            }
+
+            return sessionFactory;
+        }
+    }
+}
