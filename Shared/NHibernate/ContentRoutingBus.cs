299423155371cb66925dd18630da937140a16d49
pablocastilla <pablo.castilla@gmail.com>
2015-02-04 15:58:17 +0100

    some files missing

---
+++ Shared/NHibernate/ContentRoutingBus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace Shared.NHibernate
+{
+    public static class ContentRoutingBus
+    {
+        public static ICallback ContentRoutingSend(this IBus bus, object message)
+        {
+            return bus.Send(message);
+        }
+    }
+}
